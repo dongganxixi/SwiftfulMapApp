ada51f14b12624a5e47fba4dc2ef3d1f2f54f155
dongganxixi <ruixing.xixi@gmail.com>
2024-07-15 17:33:35 +0800

    增加位置信息的单幅图的预览

---
+++ SwiftfulMapApp/Views/LocationPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationPreviewView.swift
+//  SwiftfulMapApp
+//
+//  Created by 胡瑞兴 on 2024/7/15.
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationPreviewView()
+}
